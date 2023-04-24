233947b80bf1c50aa7624a131ecc46bf87c7cda5
Fh-Ndiritu <fhndiritu@gmail.com>
2023-04-24 15:36:15 +0300

    Namespace and version the Api

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,40 @@
+module Api
+  module V1 
+    class BooksController < ApplicationController
+
+      def index
+        render json: Book.count
+      end
+    
+      def create
+        book = Book.new(book_params)
+    
+        if book.save
+          render json: book,  status: :created
+        else
+          render json: book.errors, status: :unprocessable_entity
+        end
+    
+      end
+    
+      def destroy
+        Book.find(params[:id]).destroy!
+        head :no_content
+      end
+    
+    
+    
+      private 
+    
+      def not_destroyed(e)
+        render json: { 'errors' => e.erros}, status: :unprocessable_entity
+      end
+    
+      def book_params
+        params.require(:book).permit(:title,  :author)
+      end
+    
+    end
+    
+  end
+end
